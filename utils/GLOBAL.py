f8f0f34e7b63d5a6d2200e974bb5ce6df4323998
Zhiyi Shi <zhiyiscs@gmail.com>
2025-10-02 03:05:36 -0500

    Update GLOBAL.py

--- utils/GLOBAL.py
+++ utils/GLOBAL.py
@@ -1,4 +1,4 @@
-ROOT_PATH = '/n/holylfs05/LABS/pfister_lab/Lab/coxfs01/pfister_lab2/Lab/bjwang/projects/ICCV2025/VisEdit-main'
+ROOT_PATH = 'DualEdit-main'
 model_path_map = {
     'llava-v1.5-7b': 'models/llava-1.5-7b-hf/',
     'blip2-opt-2.7b': 'models/blip2-opt-2.7b/',
